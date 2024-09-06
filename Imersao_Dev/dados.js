afc3e29fe4e25be82df6e979ffffa371ccab799e
MatDiniz15 <127975435+MatDiniz15@users.noreply.github.com>
2024-09-06 15:59:43 -0300

    Add files via upload

---
+++ Imersao_Dev/dados.js
@@ -0,0 +1,51 @@
+let personagens = [
+    {
+      nome: "Frodo Bolseiro",
+      raca: "Hobbit",
+      classe: "Portador do Anel",
+      habilidades: ["Resistência ao Anel", "Coragem"],
+      equipamento: ["O Um Anel", "Faca de Ferro élfico", "Mithril"],
+      biografia: "Frodo é um hobbit da Terra Média que herda o Um Anel de seu tio Bilbo. Ele é encarregado de destruir o Anel na Montanha da Perdição."
+    },
+    {
+      nome: "Gandalf",
+      raca: "Maiar",
+      classe: "Mago",
+      habilidades: ["Magia", "Sabedoria", "Liderança"],
+      equipamento: ["Cajado", "Glamdring", "Narya"],
+      biografia: "Gandalf é um dos Istari enviados à Terra Média para ajudar na luta contra Sauron. Ele é um guia e mentor para a Sociedade do Anel."
+    },
+    {
+      nome: "Aragorn",
+      raca: "Humano",
+      classe: "Ranger",
+      habilidades: ["Combate com espada", "Liderança", "Rastreamento"],
+      equipamento: ["Andúril", "Arco e flechas", "Anel de Barahir"],
+      biografia: "Aragorn é o herdeiro de Isildur e o legítimo rei de Gondor. Ele é um guerreiro habilidoso e líder da Sociedade do Anel."
+    },
+    {
+      nome: "Legolas",
+      raca: "Elfo",
+      classe: "Arqueiro",
+      habilidades: ["Tiro com arco", "Agilidade", "Visão aguçada"],
+      equipamento: ["Arco élfico", "Adagas"],
+      biografia: "Legolas é um príncipe elfo da Floresta das Trevas e um membro da Sociedade do Anel. Ele é conhecido por sua habilidade com o arco e sua agilidade."
+    },
+    {
+      nome: "Gimli",
+      raca: "Anão",
+      classe: "Guerreiro",
+      habilidades: ["Combate com machado", "Força", "Resistência"],
+      equipamento: ["Machado de batalha", "Cota de malha"],
+      biografia: "Gimli é um anão da Montanha Solitária e um membro da Sociedade do Anel. Ele é um guerreiro valente e leal."
+    },
+    {
+      nome: "Boromir",
+      raca: "Humano",
+      classe: "Guerreiro",
+      habilidades: ["Combate com espada", "Liderança", "Força"],
+      equipamento: ["Espada", "Escudo", "Chifre de Gondor"],
+      biografia: "Boromir é o filho mais velho do Regente de Gondor. Ele é um guerreiro corajoso, mas sua ambição pelo poder do Anel leva à sua queda."
+    }
+  ];
+  
